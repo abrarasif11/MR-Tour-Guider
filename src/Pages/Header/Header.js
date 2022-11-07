9bc704616cc2d24ac43dfe2747f8e6b404904c31
abrarasif11 <faasif929@gmail.com>
2022-11-07 23:05:48 +0600

    footer-added

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -5,10 +5,10 @@ const Header = () => {
     return (
         <header className="p-4 bg-black text-green-700">
 	<div className="container flex justify-between h-16 mx-auto">
-    <a rel="noopener noreferrer" href="/home" aria-label="Back to homepage" className="flex items-center p-2">
+    <Link rel="noopener noreferrer" to="/" aria-label="Back to homepage" className="flex items-center p-2">
 			<h2 className='font-poppins text-4xl font-bold'>Travel Hunter</h2>
 			<img className='ps-10 w-10' src="https://www.pinclipart.com/picdir/big/29-290936_location-clipart-travel-tourism-world-travel-logo-png.png" alt="" />
-		</a>
+		</Link>
 		<ul className="items-stretch hidden space-x-3 md:flex">
         <li className="flex">
 				<Link rel="noopener noreferrer" to="/" className="flex items-center px-4 -mb-1 border-b-2 border-transparent">Home</Link>
