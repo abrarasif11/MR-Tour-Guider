aa211eae91eaf1c7e9da6f3776221686b2223e55
abrarasif11 <faasif929@gmail.com>
2022-11-09 21:33:00 +0600

    react-photo-view

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import photo from '../assets/travel-icon--be-found-online-9.png'
 
 const Header = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,21 +13,9 @@ const Header = () => {
 			  to="/"
 			  class="inline-flex items-center"
 			>
-			  <svg
-				class="w-8 text-teal-accent-400"
-				viewBox="0 0 24 24"
-				strokeLinejoin="round"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeMiterlimit="10"
-				stroke="currentColor"
-				fill="none"
-			  >
-				<rect x="3" y="1" width="7" height="12" />
-				<rect x="3" y="17" width="7" height="6" />
-				<rect x="14" y="1" width="7" height="6" />
-				<rect x="14" y="11" width="7" height="12" />
-			  </svg>
+			 <div className="w-20">
+				<img src={photo} alt="" />
+			 </div>
 			  
 			  <span class="ml-2 font-poppins text-xl font-bold tracking-wide text-green-700 uppercase">
 				MR. Tour Guider
